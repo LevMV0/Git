1c084b17f9849aa57d68ded250c4bfb11a44d1ef
LevMV <masha.levitskaya2000@gmail.com>
2025-12-26 14:23:40 +0200

    classworktasks1

---
+++ classwork2.py
@@ -0,0 +1,28 @@
+#1
+num1 = float (input("Enter first number"))
+num2 = float(input("Enter second number"))
+print (f"num1 + num2 = {num1 + num2}")
+print (f"num1 - num2 = {num1 - num2}")
+print (f"num1 * num2 = {num1 * num2}")
+#2
+num1 = float (input("Enter first number"))
+num2 = float(input("Enter second number"))
+print (f"(num1 * num2)/100 = {(num1 * num2)/100}")
+#3
+a = float (input("Введіть довжину першої сторони прямокутника"))
+b = float(input("Введіть довжину другої сторони прямокутника"))
+print (f"(num1 + num2)*2 = {(num1 + num2)*2}")
+#4
+a = float (input("Введіть довжину сторони квадрату"))
+P = float
+P = 4*a
+print ("P=",P)
+#5
+num1 = float (input("Enter number"))
+num2 = num1 * 100
+print ("num2 =", num2)
+#6
+num1 = float (input("Введіть кількість фарби для 1 стіни"))
+num2 = 4*num1
+print ("Для фарбування 4 стін потрібно", num2 , "літрів фарби")
+ 
